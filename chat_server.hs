c5d82b85c5f92ffd987a76650d8e9cbb9f3cf74f
Sujan Nag <nags@tcd.ie>
2017-11-21 22:50:10 +0000

    Basic skeleton ready

--- chat_server.hs
+++ chat_server.hs
@@ -2,9 +2,18 @@ module Main where
 
 import Network.Socket
 import System.IO
+import Control.Concurrent
+import Control.Exception
+import Control.Monad (when)
+import Control.Monad.Fix (fix)
+
+ -- Code added to facilitate communication between threads
 
 main :: IO()
 main = do
+	-- let value = 10
+	--putStrLn ("Hello!!" ++ value)
+
 	-- create a socket
 	sock <- socket AF_INET Stream 0
 
@@ -18,27 +27,69 @@ main = do
 	listen sock 2
 
 	-- Doesnt return, keep on listening iteratively.
-	mainLoop sock
+	-- newChan :: IO (Chan a)
+	chan <- newChan		--newChan builds and returns a new instance of Chan
+	
+	-- why?
+	_ <- forkIO $ fix $ \loop -> do
+		(_, _) <- readChan chan
+		loop
+	mainLoop sock chan 0
+
+type Msg = (Int, String)
 
 -- mainLoop function
-mainLoop :: Socket -> IO()
-mainLoop sock = do		-- takes sock as an argument
+mainLoop :: Socket -> Chan Msg -> Int -> IO()
+mainLoop sock chan msgNum = do		-- takes sock as an argument
 	-- accept a socket connection
 	conn <- accept sock
 
 	-- Server logic. It also closes the socket
-	runConn conn
+	forkIO (runConn conn chan msgNum)	-- each connection starts in its own thread
 
 	-- repeat
-	mainLoop sock
+	mainLoop sock chan $! msgNum + 1
 
 -- runConn function
-runConn :: (Socket, SockAddr) -> IO ()
+runConn :: (Socket, SockAddr) -> Chan Msg  -> Int ->IO ()
 
 -- use system IOs instead of send and recv
-runConn (sock, _) = do
-	hdl <- socketToHandle sock ReadWriteMode
-	hSetBuffering hdl NoBuffering
-	hPutStrLn hdl "Hello!"
-	hClose hdl
+runConn (sock, _) chan msgNum = do
+
+	-- writeChan :: Chan a -> a -> IO ()
+	-- Write a value to Chan, in our case msg to chan
+	let broadcast msg = writeChan chan (msgNum, msg)
+	hdl <- socketToHandle sock ReadWriteMode	--no idea
+	hSetBuffering hdl NoBuffering			--no idea
+	
+	-- Send print messages
+	hPutStrLn hdl "Hi, what's your name?"
+	name <- fmap init (hGetLine hdl)
+	broadcast ("---> " ++ name ++ " entered chat.")
+	hPutStrLn hdl ("Welcome, " ++ name ++ "!")
+
+	-- Duplicate a channel, Data written by anyone is seen by everyone else.
+	-- dupChan :: Chan a -> IO (Chan a)
+	commLine <- dupChan chan
+
+	--fork off a thread for reading from the duplicate channel
+	reader <- forkIO $ fix $ \loop -> do
+		--line <- fmap init(hGetLine hdl)		-- no idea
+		(nextNum, line) <- readChan commLine
+		when (msgNum /= nextNum) $ hPutStrLn hdl line
+		--hPutStrLn hdl line
+		loop
+	
+	-- handle exceptions
+	handle(\(SomeException _) -> return()) $ fix $ \loop -> do
+		line <- fmap init (hGetLine hdl)
+		case line of 
+			-- if an exception is caought, send a message
+			"quit" -> hPutStrLn hdl "Bye!"
+			--else loop
+			_ -> broadcast (name ++ ": " ++ line) >> loop
+
+	killThread reader                      -- kill after the loop ends
+    	broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
+    	hClose hdl                             -- close the handle
 
